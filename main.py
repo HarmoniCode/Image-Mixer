530bc11297b203a781bffa643889c2f9f456fa67
ahmed mhmoud abdelaal <ahmedmhmouad41@gmail.com>
2024-11-30 15:21:57 +0200

    image/magnitude/phase viewpython

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 import cv2
 from PyQt5.QtWidgets import (
-    QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, QVBoxLayout, QHBoxLayout, QWidget
+    QApplication, QMainWindow, QLabel, QFileDialog, QVBoxLayout, QHBoxLayout, QWidget, QComboBox
 )
 from PyQt5.QtGui import QPixmap, QImage
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -12,7 +12,7 @@ from PyQt5.QtCore import Qt
 
 class ImageGroup(QWidget):
     def __init__(self, parent=None):
-        super().__init__(parent) 
+        super().__init__(parent)
         self.layout = QVBoxLayout()
 
         self.label = QLabel("Image", self)
@@ -22,19 +22,37 @@ class ImageGroup(QWidget):
         self.label.setStyleSheet("border: 1px solid black;")
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.label.setObjectName("image_label")  
+        self.label.setObjectName("image_label")
 
-        self.magnitude_canvas = FigureCanvas(Figure(figsize=(4, 4)))
-        self.phase_canvas = FigureCanvas(Figure(figsize=(4, 4)))
+        self.combo_box = QComboBox(self)
+        self.combo_box.addItem("Magnitude")
+        self.combo_box.addItem("Phase")
+        self.combo_box.currentIndexChanged.connect(self.update_canvas)
+
+        self.canvas = FigureCanvas(Figure(figsize=(4, 4)))
 
         self.layout.addWidget(self.label)
+        self.layout.addWidget(self.combo_box)
+        self.layout.addWidget(self.canvas)
 
-        self.h_layout = QHBoxLayout()
-        self.h_layout.addLayout(self.layout)
-        self.h_layout.addWidget(self.magnitude_canvas)
-        self.h_layout.addWidget(self.phase_canvas)
+        self.setLayout(self.layout)
 
-        self.setLayout(self.h_layout)
+        self.magnitude_spectrum = None
+        self.phase_spectrum = None
+
+    def update_canvas(self):
+        if self.magnitude_spectrum is not None and self.phase_spectrum is not None:
+            if self.combo_box.currentText() == "Magnitude":
+                self.show_fft(self.magnitude_spectrum)
+            else:
+                self.show_fft(self.phase_spectrum)
+
+    def show_fft(self, spectrum):
+        self.canvas.figure.clear()
+        ax = self.canvas.figure.add_subplot(111)
+        ax.imshow(spectrum, cmap='gray')
+        ax.axis('off')
+        self.canvas.draw()
 
 
 class MainWidget(QMainWindow):
@@ -60,19 +78,10 @@ class MainWidget(QMainWindow):
         h_layout2.addWidget(self.image_group3)
         h_layout2.addWidget(self.image_group4)
 
-
-        self.image_group1.label.mouseDoubleClickEvent = lambda event: self.load_image(
-            self.image_group1.label, self.image_group1.magnitude_canvas, self.image_group1.phase_canvas
-        )
-        self.image_group2.label.mouseDoubleClickEvent = lambda event: self.load_image(
-            self.image_group2.label, self.image_group2.magnitude_canvas, self.image_group2.phase_canvas
-        )
-        self.image_group3.label.mouseDoubleClickEvent = lambda event: self.load_image(
-            self.image_group3.label, self.image_group3.magnitude_canvas, self.image_group3.phase_canvas
-        )
-        self.image_group4.label.mouseDoubleClickEvent = lambda event: self.load_image(
-            self.image_group4.label, self.image_group4.magnitude_canvas, self.image_group4.phase_canvas
-        )
+        self.image_group1.label.mouseDoubleClickEvent = lambda event: self.load_image(self.image_group1)
+        self.image_group2.label.mouseDoubleClickEvent = lambda event: self.load_image(self.image_group2)
+        self.image_group3.label.mouseDoubleClickEvent = lambda event: self.load_image(self.image_group3)
+        self.image_group4.label.mouseDoubleClickEvent = lambda event: self.load_image(self.image_group4)
 
         main_layout = QVBoxLayout()
         main_layout.addLayout(h_layout1)
@@ -82,19 +91,18 @@ class MainWidget(QMainWindow):
         container.setLayout(main_layout)
         self.setCentralWidget(container)
 
-    def load_image(self, label, mag_canvas, phase_canvas):
+    def load_image(self, image_group):
         file_path, _ = QFileDialog.getOpenFileName(self, "Open Image File", "", "Images (*.png *.jpg *.jpeg *.bmp *.tiff)")
         if file_path:
             image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-            self.show_image(image, label)
+            self.show_image(image, image_group.label)
 
             f = np.fft.fft2(image)
             fshift = np.fft.fftshift(f)
-            magnitude_spectrum = 20 * np.log(np.abs(fshift))
-            phase_spectrum = np.angle(fshift)
+            image_group.magnitude_spectrum = 20 * np.log(np.abs(fshift))
+            image_group.phase_spectrum = np.angle(fshift)
 
-            self.show_fft(magnitude_spectrum, mag_canvas)
-            self.show_fft(phase_spectrum, phase_canvas)
+            image_group.update_canvas()
 
     def show_image(self, image, label):
         h, w = image.shape
@@ -103,12 +111,6 @@ class MainWidget(QMainWindow):
         label.setPixmap(pixmap)
         label.setScaledContents(True)
 
-    def show_fft(self, spectrum, canvas):
-        canvas.figure.clear()
-        ax = canvas.figure.add_subplot(111)
-        ax.imshow(spectrum, cmap='gray')
-        ax.axis('off')
-        canvas.draw()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

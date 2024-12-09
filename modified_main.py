4f837fee1860997c527860c1bf3449f3b0b1e8f1
AdhamMahran <adhammahran2003@gmail.com>
2024-12-09 17:32:16 +0200

    Added Default Values

--- modified_main.py
+++ modified_main.py
@@ -168,10 +168,10 @@ class MainWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
-        
+
     def initUI(self):
         self.setWindowTitle("Image FFT Viewer")
-        self.setGeometry(100, 100, 600, 800)  
+        self.setGeometry(100, 100, 600, 800)
 
         self.image_group1 = ImageGroup(self)
         self.image_group2 = ImageGroup(self)
@@ -188,25 +188,25 @@ class MainWidget(QMainWindow):
         self.image_group2.label.mouseDoubleClickEvent = lambda event: self.load_image(self.image_group2)
         self.image_group3.label.mouseDoubleClickEvent = lambda event: self.load_image(self.image_group3)
         self.image_group4.label.mouseDoubleClickEvent = lambda event: self.load_image(self.image_group4)
-     
+
         self.out_port_1 = OutPort(self, "Port 1")
         self.out_port_2 = OutPort(self, "Port 2")
         self.out_port_1.radio.setChecked(True)
         self.out_port_2.radio.setChecked(False)
-      
+
         v_layout2 = QVBoxLayout()
         v_layout2.addWidget(self.out_port_1)
         v_layout2.addWidget(self.out_port_2)
-        
+
         self.progress_bar = QProgressBar()
         self.progress_bar.setValue(0)
         mix_button = QPushButton("Mix")
         mix_button.clicked.connect(self.mix_images)
-        
+
         self.outport_group = QButtonGroup(self)
         self.mode_group = QButtonGroup(self)
         self.region_group = QButtonGroup(self)
-        
+
         self.outport_group.addButton(self.out_port_1.radio)
         self.outport_group.addButton(self.out_port_2.radio)
 
@@ -214,23 +214,23 @@ class MainWidget(QMainWindow):
         self.mode_radio_1.setChecked(True)
 
         self.mode_group.addButton(self.mode_radio_1)
-        
+
         self.mode_radio_2 = QRadioButton("Real / Imaginary", self)
         self.mode_radio_2.setChecked(False)
 
         self.mode_group.addButton(self.mode_radio_2)
         v_layout2.addWidget(self.mode_radio_1)
         v_layout2.addWidget(self.mode_radio_2)
-        
+
         self.inside_region_radio = QRadioButton("Region inside", self)
         self.inside_region_radio.setChecked(True)
         self.region_group.addButton(self.inside_region_radio)
-        
+
         self.outside_region_radio = QRadioButton("Region outside", self)
         self.outside_region_radio.setChecked(False)
 
         self.region_group.addButton(self.outside_region_radio)
-       
+
         v_layout2.addWidget(self.inside_region_radio)
         v_layout2.addWidget(self.outside_region_radio)
 
@@ -240,7 +240,7 @@ class MainWidget(QMainWindow):
         main_layout = QHBoxLayout()
         main_layout.addLayout(v_layout1)
         main_layout.addLayout(v_layout2)
-        
+
         container = QWidget()
         container.setLayout(main_layout)
         self.setCentralWidget(container)
@@ -255,6 +255,7 @@ class MainWidget(QMainWindow):
         self.image_group2.rectangle_selector.onselect = self.on_select
         self.image_group3.rectangle_selector.onselect = self.on_select
         self.image_group4.rectangle_selector.onselect = self.on_select
+        self.load_images_from_folder()
 
     def on_select(self, eclick, erelease):
         x0, y0 = eclick.xdata, eclick.ydata
@@ -283,9 +284,39 @@ class MainWidget(QMainWindow):
                 image_group.rectangle_selector.extents = (x0, x1, y0, y1)
                 image_group.rectangle_selector.update()
 
+    def load_images_from_folder(self):
+        image_files = [
+            "Data/image1.jpg",
+            "Data/image2.jpg",
+            "Data/image3.jpg",
+            "Data/image4.jpg"
+        ]
+        image_groups = [
+            self.image_group1,
+            self.image_group2,
+            self.image_group3,
+            self.image_group4
+        ]
+
+        for file_path, image_group in zip(image_files, image_groups):
+            if file_path:
+                image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+
+                # Resize the image to a common shape (e.g., 256x256)
+                common_shape = (256, 256)
+                image = cv2.resize(image, common_shape)
+
+                image_group.set_image(image)
+
+                f = np.fft.fft2(image)
+                fshift = np.fft.fftshift(f)
+                image_group.magnitude_spectrum = 20 * np.log(np.abs(fshift) + 1e-5)
+                image_group.phase_spectrum = np.angle(fshift)
+
+                image_group.update_canvas()
     def load_image(self, image_group):
         file_path, _ = QFileDialog.getOpenFileName(self, "Open Image File", "",
-                                                   "Images (*.png *.jpg *.jpeg *.bmp *.tiff)")
+                                                   " (*.png *.jpg *.jpeg *.bmp *.tiff)")
         if file_path:
             image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
 
@@ -341,14 +372,13 @@ class MainWidget(QMainWindow):
                 print("Weights sum is zero. Cannot divide by zero.")
                 return
 
-            normalized_weights = [weight / weights_sum for weight in [weight_1, weight_2, weight_3, weight_4]]
+            normalized_weights = [ 2 * weight / weights_sum for weight in [weight_1, weight_2, weight_3, weight_4]]
 
             # Ensure selected_region is set
             if self.selected_region is None:
-                print("No region selected.")
-                return
-
-            y_min, y_max, x_min, x_max = self.selected_region
+                y_min, y_max, x_min, x_max = 0, 255, 0, 255
+            else:
+                y_min, y_max, x_min, x_max = self.selected_region
 
             # Process image groups
             for group, weight in zip(
